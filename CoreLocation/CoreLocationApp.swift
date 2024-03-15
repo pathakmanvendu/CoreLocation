ab4b125a016d4f7707baf98379cdd7f36acad210
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-03-15 14:25:30 +0530

    Initial Commit

---
+++ CoreLocation/CoreLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreLocationApp.swift
+//  CoreLocation
+//
+//  Created by Manvendu Pathak on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
